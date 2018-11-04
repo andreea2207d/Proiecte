b2f5f7702e758c38fb2f4021c6c72293de86df85
Andreea <andreea_2207d@yahoo.com>
2018-11-04 16:31:07 +0200

    - ex 1,7

--- Project1/Project1/Rational.cpp
+++ Project1/Project1/Rational.cpp
@@ -1,7 +1,50 @@
 #include "Rational.hpp"
 
-class Rational {
+void Rational::doCanonicalForm()
+{
+	// Divide both numbers with greatest common divisior
 
-private:
-	int _denominator, _numerator;
-};
+	int smallest = _numerator < _denominator ? _numerator : _denominator;
+	int gcd = 1;
+
+	for (int i = 2; i < smallest; i++) {
+		if (_numerator % i == 0 && _denominator % i == 0) {
+			gcd = i;
+		}
+	}
+
+	_numerator /= gcd;
+	_denominator /= gcd;
+}
+
+void Rational::setFraction(int denom, int num)
+{
+	_denominator = denom;
+	_numerator = num;
+
+	doCanonicalForm();
+}
+
+void Rational::setNum(int num)
+{
+	_numerator = num;
+
+	doCanonicalForm();
+}
+
+void Rational::setDenom(int denom)
+{
+	_denominator = denom;
+
+	doCanonicalForm();
+}
+
+int Rational::getDenom()
+{
+	return _denominator;
+}
+
+int Rational::getNum()
+{
+	return _numerator;
+}
--- Project1/Project1/Rational.hpp
+++ Project1/Project1/Rational.hpp
@@ -1,5 +1,14 @@
-class Rational {
+#pragma once
 
+class Rational 
+{
+public:
+	void doCanonicalForm();
+	void setFraction(int denom, int num);
+	void setDenom(int denom);
+	void setNum(int num);
+	int getDenom();
+	int getNum();
 private:
 	int _denominator, _numerator;
 };
